9b077a0caad306e2df1c9cdbe3525bfd0efb336c
Aleh <oleh@tut.by>
2021-06-03 18:34:08 +0300

    1

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,7 @@ import { Game } from "./components/game/game";
 import { ImageCategoryModel } from "./models/image-category-model"
 
 export class App {
-
     private readonly game: Game;
-
     constructor(private readonly rootElement: HTMLElement) {
         this.game = new Game();
         this.rootElement.appendChild(this.game.element);
